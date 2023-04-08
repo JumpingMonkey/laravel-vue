cbd25cf117cf4005e7fd8c41ed503d3544b25db8
AlexMrz <morozovprav@gmail.com>
2023-04-08 21:43:54 +0100

    add style

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,8 +1,11 @@
 <template>    
-    <div v-for="listing in listings" :key="listing.id">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
         <div>
             <Link :href="route('listing.show', {listing: listing.id})">
-                <ListingAddress :listing="listing"></ListingAddress>
+                <Price :price="listing.price" class="text-2xl font-bold"></Price>
+                <ListingSpace :listing="listing" class="text-lg"></ListingSpace>
+                <ListingAddress :listing="listing" class="text-gray-500"></ListingAddress>
             </Link>
         </div>
         <div>
@@ -11,12 +14,16 @@
         <div>
             <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
         </div>
-     </div>
+    </Box>
+    </div>
 </template>
 
 <script setup>
 import {Link} from "@inertiajs/vue3"
-import ListingAddress from "../../Components/ListingAddress.vue";
+import Price from "@/Components/Price.vue"
+import Box from "@/Components/UI/Box.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import ListingAddress from "@/Components/ListingAddress.vue";
     defineProps({
         listings: Array,
     })
