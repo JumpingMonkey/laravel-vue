4a3fb43917c594e3ee411d0258ab7d59d370578f
AlexMrz <morozovprav@gmail.com>
2023-04-22 22:14:09 +0100

    add listing policy logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $user2 = User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'test2@example.com',
+        ]);
         Listing::factory(10)->create([
             'by_user_id' => 1,
         ]);
