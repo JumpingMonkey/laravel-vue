4a047be43c3cb22575186ce5918f958d5a9277b2
AlexMrz <morozovprav@gmail.com>
2023-05-10 22:49:42 +0100

    add Vue form logic to files uploading

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,11 +1,10 @@
 <template>
     <Box>
         <template #header>Upload images</template>
-        <form
-            enctype="multipart/form-data"
-            method="POST" :action="route('realtor.listing.image.store', {listing: listing})">
-            <input type="file" multiple name="images[]" />
-            <button type="submit">Send</button>
+        <form @submit.prevent="upload">
+            <input type="file" multiple @input="addFiles" />
+            <button type="submit" class="btn-outline">Upload</button>
+            <button type="reset" @click="reset" class="btn-outline">Reset</button>
         </form>
     </Box>
 
@@ -13,7 +12,26 @@
 
 <script setup>
 import Box from '@/Components/UI/Box.vue'
-    defineProps({
+import { useForm } from '@inertiajs/vue3'
+    const prop = defineProps({
         listing: Object
     })
+const form = useForm({
+    images: []
+})
+
+const upload = form.post(route('realtor.listing.image.store', {listing: prop.listing}),
+        {
+            onSuccess: () => form.reset('images')
+        }
+    )
+
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image)
+    }
+}
+
+const reset = () => form.reset('images')
+
 </script>
