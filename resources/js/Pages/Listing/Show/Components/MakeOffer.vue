54afca91e25668c6758211b61a2523b38e56b20d
AlexMrz <morozovprav@gmail.com>
2023-05-25 20:46:52 +0100

    add component MakeOffer.vue and interation it into Show(listing) component

---
+++ resources/js/Pages/Listing/Show/Components/MakeOffer.vue
@@ -0,0 +1,46 @@
+<template>
+    <Box>
+        <template #header>
+            Make an Offer
+        </template>
+        <div>
+            <form>
+                <!-- <label class="label" for="amount">Amount</label> -->
+                <input v-model.number="form.amount" class="input" id="amount" type="text" />
+                <!-- <label class="label">Interest rate ({{ interestRate }}%)</label> -->
+                <input v-model.number="form.amount" type="range" :min="min" :max="max" step="10000"
+                        class="input-range mt-2" />
+                <button type="submit" class="btn-outline w-full mt-2 text-sm">
+                    Make an Offer
+                </button>
+            </form>
+        </div>
+        <div class="flex justify-between text-gray-500 mt-2">
+            <div>Difference</div>
+            <div>
+                <Price :price="difference"></Price>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script setup>
+import Box from '@/Components/UI/Box.vue';
+import Price from '@/Components/Price.vue';
+import { useForm } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const props = defineProps({
+    listingId: Number,
+    price: Number,
+
+})
+const form = useForm({
+    amount: props.price,
+})
+
+const difference = computed(() => form.amount - props.price);
+const min = computed(() => props.price / 2);
+const max = computed(() => props.price * 2);
+
+</script>
