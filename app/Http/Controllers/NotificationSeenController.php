491d050fa94f601649e35f1fabfedf0b74b337e5
AlexMrz <morozovprav@gmail.com>
2023-06-16 22:16:15 +0100

    add notification seen controller ad route

---
+++ app/Http/Controllers/NotificationSeenController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Notifications\DatabaseNotification;
+
+class NotificationSeenController extends Controller
+{
+    public function __invoke(DatabaseNotification $notification)
+    {
+        $notification->markAsRead();
+
+        return redirect()->back()
+            ->with('success', 'Marked as read!');
+    }
+}
